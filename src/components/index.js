fe57e5b94657d22819a0076d2b569dd49716f92f
jcassidy3280 <jcassidy3280@gmail.com>
2023-06-13 14:29:43 -0400

    index

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,14 @@
-// there are things here-
+const COHORT_NAME = "/2304-FTB-ET-WEB-FT";
+const BASE_URL = `https://strangers-things.herokuapp.com/api${COHORT_NAME}`
+
+export const fetchApi = async () => {
+    try {
+        const response = await fetch(`${BASE_URL}/posts`)
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+fetchApi()
