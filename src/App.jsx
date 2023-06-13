7e16f3d4af32378a9f29b1b65b0820b5d2c590d0
Parker-Pond <parker.a.pond@gmail.com>
2023-06-13 12:28:52 -0600

    added routes to the homepage and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
 import './App.css'
-
+import { Routes, Route } from 'react-router-dom'
+import {fetchApi} from "./components/index"
+import {HomePage, LoginPage} from "./components/zcompConnect"
 function App() {
-  const [count, setCount] = useState(0)
+async function getInfo(){
+  try {
+    let response = await fetchApi ()
+    let data = await response.json()
+    return data
 
+  } catch (error) {
+    console.log (error)
+  }
+  getInfo()
+}
   return (
     <>
       <h1>hello world</h1>
+      <Routes>
+        <Route path="/" element={HomePage} />
+        <Route path="/login" element={LoginPage} />
+      </Routes>
+
     </>
   )
 }
