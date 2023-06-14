5e71745f2ed9fa14f5f194505353dcbde0fe19e6
Parker-Pond <parker.a.pond@gmail.com>
2023-06-14 10:00:25 -0600

    added a few things

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,18 +1,33 @@
 import React from "react";
+import { useState } from "react";
 import { CategoryBar, Post } from "../zcompConnect";
 import "./Homepage.css";
 
 export default function HomePage() {
+  const [searchedItem, setSearchedItem] = useState("");
   return (
     <div>
-      <p>Home Page</p>
       <div class="header">
         <img id="logo" src="falconLogo.png" />
+        <p>Home Page</p>
         <button id="loginButton">Login</button>
       </div>
       <div class="categories">
         {/* i dont know if we want to hard code in the catagories or not. i am going to hard code them in and we can change it late if needs be */}
         <CategoryBar />
+      </div>
+      <div class="searchbar">
+        <form>
+          <label>Search Bar</label>
+          <input
+            type="text"
+            value={searchedItem}
+            onChange={(e) => {
+              setSearchedItem(e.target.value);
+              console.log(searchedItem);
+            }}
+          ></input>
+        </form>
       </div>
       <div class="cardBox">
         <Post />
