c88580ba2353497d6053735da1bb054fdc50424e
Parker-Pond <parker.a.pond@gmail.com>
2023-06-14 16:46:36 -0600

    login page fundementals as beginnings of api connectivity

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,10 +1,49 @@
+import { useState, useEffect } from "react";
+const COHORT_NAME = "2304-FTB-ET-WEB-FT";
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
 
 
-export default function LoginPage (){
+export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    return(
-        <div>
-            <h1>Login Page</h1>
-        </div>
-    )
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    console.log("submit worked YAY")
+  }
+
+  useEffect (()=>{
+    async () =>{
+        try {
+            const results = await fetch(`${BASE_URL}/users/register`);
+            
+        } catch (error) {
+            console.log("LoginPage register ", error)
+        }
+    }
+  })
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
