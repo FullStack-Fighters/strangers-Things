2f2032b52597c8e90d4d91d72823783e3c8d06c4
jcassidy3280 <jcassidy3280@gmail.com>
2023-06-19 11:00:32 -0400

    My Profile is displaying. Messages does not work

--- src/components/PersonalProfilePage.jsx
+++ src/components/PersonalProfilePage.jsx
@@ -9,6 +9,7 @@ const MyProfile = () => {
     const [myId, setMyId] = useState("")
     const [myUsername, setMyUsername] = useState("")
     const [myMessages, setMyMessages] = useState([])
+    const [myProfileData, setMyProfileData] = useState([])
     
     const myUserData = async () => {
     try {
@@ -19,34 +20,51 @@ const MyProfile = () => {
         },
       });
       const result = await response.json();
-      setMyPosts(result.data.posts)
-      setMyId(result.data._id) 
-      setMyUsername(result.data.username)
-      setMyMessages(result.data.myMessages)
-
+      return result
+            
     } catch (error) {
-      console.log(error);
+        console.log(error);
     } 
+}
+
+useEffect(() => {
+    const getUserData = async () => {
+        try {
+            const response = await myUserData();
+            console.log(response)
+            setMyProfileData(response.data);
+            setMyPosts(response.data.posts)
+            setMyId(response.data._id) 
+            setMyUsername(response.data.username)
+            setMyMessages(response.data.messages)
+    } catch (error) {
+    console.log(error);
     }
-    myUserData()
+    };
+    getUserData();
+  }, []);
+
     return (
         <>
         <div>
-            <p>Hello {myUsername}</p>
-            {/* <p>Posts {myPosts}</p>
-            <p>Messages {myMessages}</p> */}
+            <h2>Hello {myUsername}</h2>
             {console.log(myMessages)}
-
-            {/* <div>
+            <div>
                 {
-                    myMessages.length  ?                   
-                    myMessages : <p>You have no messages.</p>
+                    myMessages && myMessages.length  ? (myMessages.map((message, idx) =>{
+                        return (
+                            <div key={idx}>
+                                <h2>Your Messages</h2>
+                                <p>{message}</p>
+
+                            </div>
+                        )
+                    }))                 
+                 : <p>You have no messages.</p>
                 }
-            </div> */}
+            </div>
         </div>
-        <div>
-            <NewMessages />
-        </div>
+
         </>
     )
 }
